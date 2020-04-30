f6f4dd14ad985e36e94f2f9f8474c35a1be60e6d
RRToast <43878153+RRToast@users.noreply.github.com>
2020-04-30 12:14:59 +0200

    Update Frequcy.c

--- Frequency/Frequcy.c
+++ Frequency/Frequcy.c
@@ -12,17 +12,19 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
-
 int main(void) {
-	struct timespec abs_start_time;
-	struct timespec end_time;
-	struct timespec start_time;
+	struct timespec abs_start_time;	 // Anfangszeit
+	struct timespec end_time;	// Endzeit
+	struct timespec start_time;	//1Ms Takt, wird bei jeder schleife um eine MS erhöht
 	int long erwartung;
-	int long max =0;
-	int long min = 1999999999;
+	int long max = 0; // größte Wartezeit zwischen zwei Takten
+	int long min = 1999999999;	//geringste Wartezeit zwischen zwei Takten
+	int long LOOP_ITERATIONS = 10000;
+	int long MIO = 1000000;
+	int long WRAP_AROUNT = 999000000;	// 1Mrd wird zu 0 => Wraparound detektiert 1Mrd - 1Mio, eine Iteration vor Wraparound
 	struct timespec validate_time;
-	long int validate_nsec[10000];
-	long int validate_sec[10000];
+	long int validate_nsec[LOOP_ITERATIONS]; //validiere die verstrichenen NS
+	long int validate_sec[LOOP_ITERATIONS]; //validiere die verstrichenen SEc
 
 	if (-1 == clock_gettime(CLOCK_REALTIME, &start_time)){
 		perror ("Wir haben die Uhr falsch gelesen");
@@ -30,13 +32,13 @@ int main(void) {
 	}
 	abs_start_time = start_time;
 
-	for (int i = 0; i < 10000; ++i)
+	for (int i = 0; i < LOOP_ITERATIONS; ++i)
 	{
-		if (start_time.tv_nsec >= 999000000 ){
+		if (start_time.tv_nsec >= WRAP_AROUNT ){
 			start_time.tv_sec += 1;
-			start_time.tv_nsec = start_time.tv_nsec %  999000000;
+			start_time.tv_nsec = start_time.tv_nsec %  WRAP_AROUNT;
 		} else {
-			start_time.tv_nsec += 1000000;
+			start_time.tv_nsec += MIO;
 		}
 
     	if (-1 == clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &start_time, NULL)){
@@ -60,9 +62,9 @@ int main(void) {
 	printf("Ende in sec %ld und msec %ld \n \n", end_time.tv_sec - abs_start_time.tv_sec, end_time.tv_nsec - abs_start_time.tv_nsec);
 
 
-	for (int i = 1; i < 10000; ++i){
+	for (int i = 1; i < LOOP_ITERATIONS; ++i){
 		if(validate_sec[i] > validate_sec[i-1]){
-		//	erwartung = (validate_nsec[i] + 9990000000) - (validate_nsec[i-1]);
+			erwartung = (validate_nsec[i] + WRAP_AROUNT + MIO) - (validate_nsec[i-1]);
 			printf("An stelle i = %d", i);
 			printf("   validate_nscec = %ld     validate_nsec-1 = %ld \n", validate_nsec[i],validate_nsec[i-1]);
 		} else {
@@ -70,7 +72,11 @@ int main(void) {
 		}
 
 		if(erwartung > max){
+			printf("alt = %ld", max);
 			max = erwartung;
+			printf("****** An stelle i = %d", i);
+			printf(" # max wurde überschritten  : validate_nscec = %ld     validate_nsec-1 = %ld ### neuer max  = %ld \n", validate_nsec[i],validate_nsec[i-1], max);
+
 		} else if (erwartung < min){
 			printf("alt = %ld", min);
 			min = erwartung;
